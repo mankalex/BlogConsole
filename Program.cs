e8528ced4f3769ee9663acbcb81575bf7c09b025
alexm <amanka@my.wctc.edu>
2023-04-10 14:35:57 -0500

    [1.09] Added Create Post and Display Posts

--- Program.cs
+++ Program.cs
@@ -17,25 +17,24 @@ try
     Console.WriteLine("Enter to quit");
     //Prompt user for input
     string choice = Console.ReadLine();
-    var name = "";
 
     if(choice == "1")
     {
         // Display all Blogs from the database
         var db = new BloggingContext();
-        var query = db.Blogs.OrderBy(b => b.Name);
+        var query = db.Blogs.OrderBy(b => b.BlogId);
 
         Console.WriteLine("All blogs in the database:");
         foreach (var item in query)
         {
-            Console.WriteLine(item.Name);
+            Console.WriteLine(item.BlogId + ") " + item.Name);
         }
     }
     else if(choice == "2")
     {
         // Create and save a new Blog
         Console.Write("Enter a name for a new Blog: ");
-        name = Console.ReadLine();
+        var name = Console.ReadLine();
 
         var blog = new Blog { Name = name };
 
@@ -43,7 +42,98 @@ try
         db.AddBlog(blog);
         logger.Info("Blog added - {name}", name);
     }
-    
+    else if(choice == "3")
+    {
+        // Display all Blogs from the database
+        var db = new BloggingContext();
+        var query = db.Blogs.OrderBy(b => b.BlogId);
+
+        Console.WriteLine("All blogs in the database:");
+        foreach (var item in query)
+        {
+            Console.WriteLine(item.BlogId + ") " + item.Name);
+        }
+
+        //user input
+        Console.WriteLine("Enter blog you are looking to post on: ");
+        string input = Console.ReadLine();
+        int blogid;
+        //test if user input is valid
+        if(Int32.TryParse(input, out blogid))
+        {
+
+            Console.WriteLine("Etner the Title of this post: ");
+            var ptitle = Console.ReadLine();
+            if(ptitle == null || ptitle == " " || ptitle == "")
+            {
+                logger.Error("Title is null");
+            }
+
+            Console.WriteLine("Enter the Content of this post ");
+            var pcontent = Console.ReadLine();
+
+            var post = new Post { Title = ptitle, BlogId = blogid, Content = pcontent};
+
+            db.AddPost(post);
+            logger.Info("post added - {ptitle}",ptitle);
+        }
+        else{
+            logger.Error("Invalid input for blog id");
+        }
+    }
+    else if(choice == "4")
+    {
+        // Display all Posts from the database
+        var db = new BloggingContext();
+        var query = db.Blogs.OrderBy(b => b.BlogId);
+        var blogCount = db.Blogs.Count();
+
+        Console.WriteLine("Select the blog's posts to display:");
+        Console.WriteLine("0) Posts from all blogs");
+        foreach (var item in query)
+        {
+            Console.WriteLine(item.BlogId + ") Posts from " + item.Name);
+        }
+        string input = Console.ReadLine();
+        int blogid;
+        //tests user input is valid
+        if(Int32.TryParse(input, out blogid))
+        {
+            //if user input get all posts
+            if(blogid == 0)
+            {
+                var pquery = db.Posts.OrderBy(x => x.PostId);
+                foreach(var item in pquery)
+                {
+                    Console.WriteLine("BlogId: " + item.BlogId);
+                    Console.WriteLine("Title: " + item.Title);
+                    Console.WriteLine("Content: " + item.Content);
+                }
+            }
+            //if user input any specific blog id posts
+            
+            else if(blogid >=1 && blogid <=blogCount)
+            {
+                var pquery = db.Posts.Where(x => x.BlogId == blogid);
+                foreach(var item in pquery)
+                {
+                    Console.WriteLine("BlogId: " + item.BlogId);
+                    Console.WriteLine("Title: " + item.Title);
+                    Console.WriteLine("Content: " + item.Content);
+                }
+
+            }
+            else
+            {
+                logger.Error("blog id does not exist");
+            }
+            
+        }
+        else{
+            logger.Error("Invalid input for blog id");
+        }
+
+    }
     
 }
 catch (Exception ex)
