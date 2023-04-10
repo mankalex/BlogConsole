af13ee0cc55e49936a2099ec25d6e1ee510fa99b
alexm <amanka@my.wctc.edu>
2023-04-10 12:52:17 -0500

    [1.08] Added Display Blogs and Add Blog

--- Program.cs
+++ Program.cs
@@ -10,25 +10,41 @@ logger.Info("Program started");
 
 try
 {
+    Console.WriteLine("1) Display All Blogs");
+    Console.WriteLine("2) Add Blog");
+    Console.WriteLine("3) Create Post");
+    Console.WriteLine("4) Display All Posts");
+    Console.WriteLine("Enter to quit");
+    //Prompt user for input
+    string choice = Console.ReadLine();
+    var name = "";
 
-    // Create and save a new Blog
-    Console.Write("Enter a name for a new Blog: ");
-    var name = Console.ReadLine();
-
-    var blog = new Blog { Name = name };
-
-    var db = new BloggingContext();
-    db.AddBlog(blog);
-    logger.Info("Blog added - {name}", name);
-
-    // Display all Blogs from the database
-    var query = db.Blogs.OrderBy(b => b.Name);
-
-    Console.WriteLine("All blogs in the database:");
-    foreach (var item in query)
+    if(choice == "1")
     {
-        Console.WriteLine(item.Name);
+        // Display all Blogs from the database
+        var db = new BloggingContext();
+        var query = db.Blogs.OrderBy(b => b.Name);
+
+        Console.WriteLine("All blogs in the database:");
+        foreach (var item in query)
+        {
+            Console.WriteLine(item.Name);
+        }
     }
+    else if(choice == "2")
+    {
+        // Create and save a new Blog
+        Console.Write("Enter a name for a new Blog: ");
+        name = Console.ReadLine();
+
+        var blog = new Blog { Name = name };
+
+        var db = new BloggingContext();
+        db.AddBlog(blog);
+        logger.Info("Blog added - {name}", name);
+    }
+    
+    
 }
 catch (Exception ex)
 {
